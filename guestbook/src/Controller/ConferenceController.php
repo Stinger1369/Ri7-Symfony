49beb8e722d741688fa0033778bedc189e05e424
bilel zaaraoui <115618616+Dev6Socializus@users.noreply.github.com>
2023-10-03 12:05:16 +0200

    First controller

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -2,16 +2,19 @@
 
 # src/Controller/ConferenceController.php
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'conference')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        return new Response(<<<EOF
+            <html>
+                <body><img src="/images/under-construction.gif" /></body>
+            </html>
+        EOF);
     }
 }
